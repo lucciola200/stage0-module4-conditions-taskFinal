931f7b918adb717822aaa138c60b132487debccd
lucciolla200 <teoteo.odisharia@gmail.com>
2022-08-13 11:19:49 +0400

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -3,5 +3,48 @@ package school.mjc.stage0.conditions.finalTask;
 public class DaysInMonth {
     public void printDays(int year, int month) {
 
+        boolean leapYear = isLeapYear(year);
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println(31);
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println(30);
+                break;
+            case 2:
+                System.out.println(leapYear ? 29 : 28);
+                break;
+            default:
+                System.out.println("invalid date");
+                break;
+        }
+
+    }
+
+    public boolean isLeapYear(int year) {
+        boolean leap = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            } else
+                leap = true;
+        } else
+            leap = false;
+
+        return leap;
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int result = 0;
+        switch (divider) {
+            case 0:
+                System.out.println("division by zero");
+                break;
+            default:
+                result = (dividend / divider) * divider;
+                System.out.println(result == dividend ? "can be divided completely" : "cannot be divided completely");
+        }
     }
 }
